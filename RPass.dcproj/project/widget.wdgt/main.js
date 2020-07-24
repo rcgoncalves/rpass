1b0ccd6b5f14c38f90c36bc92c464db6819e1393
rcg <rcgoncalves.pt@gmail.com>
2020-07-24 19:55:45 +0200

    Adjust license text

--- RPass.dcproj/project/widget.wdgt/main.js
+++ RPass.dcproj/project/widget.wdgt/main.js
@@ -116,18 +116,18 @@ function savePreferences() {
 }
 
 /*
- * Copyright (C) 2015-2017, Rui Carlos Gonçalves (rcgoncalves.pt@gmail.com)
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
+ * This file is part of RPass.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * RPass is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * RPass is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
  * Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this program; if not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
+ * You should have received a copy of the GNU General Public License along with RPass.  If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 // Loads preferences from file.
